5aa9b1a4ae3c994616ac443af457659f870ad796
Ademola Oladapo <ademolaoladapo@gmail.com>
2026-01-22 07:08:17 +0000

    terraform demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,23 @@ terraform {
 
 provider "google" {
   credentials = file("./keys/my-cred.json")
-  project = "my-project-id"
-  region  = "us-central1"
+  project     = "dezoom-485018"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "dezoom-48501-demo-bucket" {
+  name          = "dezoom-48501-bucket"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
